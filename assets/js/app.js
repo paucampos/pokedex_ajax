7fa8a0895cc691143f139f96df1680466d88449c
Pauliih <pau.campos89@gmail.com>
2018-02-15 20:27:21 -0300

    all pokemons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,17 @@
 const $form = $('#search-form');
 const $searchField = $('#search-keyword');
-const $pokemonContainer = $('#pokemon-container');
+const $allPokeContainer = $('#allPokes');
+const $pokeContainer = $('#pokeContainer');
 let pokemon;
 
-// Le agregamos el evento submit a nuestro formulario
-$form.submit(function (e) {
-  e.preventDefault();
-  $pokemonContainer.html('');
-  pokemon = $searchField.val();
-  getPkms();
+// Le agregamos el evento keypress a nuestro formulario
+$form.on('keypress', function (e) {
+  if(e.which === 13) {
+    e.preventDefault();
+    $pokeContainer.html('');
+    pokemon = $searchField.val();
+    getPkms();
+  }
 });
 
 // Funcion para crear peticiones
@@ -31,31 +34,37 @@ function addPkms(pkms) {
   const pokeTypes = pkms.types;
   pokeTypes.forEach(function(type) {
     const nameType = type.name;
+    console.log(nameType);
   });
-  const pokeHeight = pkms.height;
-  const pokeWeight = pkms.weight;
+  const pokeHeight = (pkms.height)/10;
+  const pokeWeight = (pkms.weight)/10;
   // const pokeAbilities
   console.log(pokeName);
   console.log(pokeTypes);
-  console.log(pokeHeight);
-  console.log(pokeWeight);
+  console.log(pokeHeight + ' m');
+  console.log(pokeWeight + ' kg');
+
   console.log(pkms.abilities[0].ability.name);
 
-  let $imagen = $('<img>').attr('src', `https://pokeapi.co/media/img/${pkms.id}.png`).addClass('imgPoke');
-  // pkms.forEach(function(pokemonName) {
-  //   const title = pokemonName.headline.main;
-  //   const snippet = pokemonName.snippet;
-    
-  //   // Creamos un li con la clase articleClass y con el texto rescatado (snippet).
-  //   let $li = $('<li />').addClass('containerPoke').text(snippet);
+  let $pokeImg = $('<img>').attr('src', `https://pokeapi.co/media/img/${pkms.id}.png`).addClass('imgPoke');
 
   //   // Anexamos el li al ul
-  $pokemonContainer.append($imagen);
+  $pokeContainer.append($pokeImg);
   // }); 
-}
+};
 
-// $(() => {
+$(() => {
+  //Mostrar todos los pokemones
+  for(let i=1; i <= 151; i++){
+     $allPokeContainer.append(`<div class="col-lg-2 col-md-2 col-xs-2 poke text-center">
+      <img id="${i}" src="http://pokeapi.co/media/img/${i}.png">
+      <p>
+      </div>`);
+  }
+  $.ajax({ // MIGRAMOS el objeto de configuración de XMLHttpRequest a jQuery con Ajax
+    url: `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+  }).done(addPkms)
+  .fail(handleError);
 
-//   const $allPoke = 
-//   $pokemonContainer.append()
-// })
+  ${pkms.id}.click()
+})
